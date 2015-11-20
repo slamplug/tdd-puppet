db97ed54ded6cf603a496ad6d0d7279da4483256
Simon Lamplugh <slamplugh@hq-lap-4353.impello.co.uk>
2015-11-20 12:44:22 +0000

    add new failing test

--- modules/mymodule/spec/classes/init_spec.rb
+++ modules/mymodule/spec/classes/init_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe 'mymodule' do
-  it { should contain_class('mymodule') }
+
+  context 'test some event involving mymodule' do
+    let(:params) { {
+      :wildfly_root => 'present',
+      :wildfly_user => 'wildfly',
+      :wildfly_group => 'wildfly'
+     }}
+
+    it { should contain_class('mymodule') }
+
+    it { should contain_file('/opt/app/widlfly').with(
+          :ensure           => 'directory',
+          :owner            => 'widlfly',
+          :group            => 'wildfly',
+          :mode             => '0755'
+       )}
+  end
 end
