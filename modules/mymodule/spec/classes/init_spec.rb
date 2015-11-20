126c4f186dc8bfd2ba154f0d0ff9469972970388
Simon Lamplugh <slamplugh@hq-lap-4353.impello.co.uk>
2015-11-20 12:54:28 +0000

    add another failing test to check non-defaults

--- modules/mymodule/spec/classes/init_spec.rb
+++ modules/mymodule/spec/classes/init_spec.rb
@@ -4,7 +4,7 @@ describe 'mymodule' do
 
   context 'test some event involving mymodule' do
     let(:params) { {
-      :wildfly_root => 'present',
+      :wildfly_root => '/opt/app/wildfly',
       :wildfly_user => 'wildfly',
       :wildfly_group => 'wildfly'
      }}
@@ -15,6 +15,23 @@ describe 'mymodule' do
           :ensure           => 'directory',
           :owner            => 'widlfly',
           :group            => 'wildfly',
+          :mode             => '0755'
+       )}
+  end
+
+  context 'test some other event involving mymodule' do
+    let(:params) { {
+      :wildfly_root => '/opt/wildfly',
+      :wildfly_user => 'bob',
+      :wildfly_group => 'bob'
+     }}
+
+    it { should contain_class('mymodule') }
+
+    it { should contain_file('/opt/widlfly').with(
+          :ensure           => 'directory',
+          :owner            => 'bob',
+          :group            => 'bob',
           :mode             => '0755'
        )}
   end
