6784cb652be6ef31bc3a0d8ef33f34577a057175
Simon Lamplugh <slamplugh@hq-lap-4353.impello.co.uk>
2015-11-20 15:07:21 +0000

    add fauiling tests

--- modules/mymodule/spec/classes/init_spec.rb
+++ modules/mymodule/spec/classes/init_spec.rb
@@ -2,21 +2,27 @@ require 'spec_helper'
 
 describe 'mymodule' do
 
-  context 'test some event involving mymodule' do
+  context 'test some event involving mymodule' do    
     let(:params) { {
       :wildfly_root => '/opt/app/wildfly',
       :wildfly_user => 'wildfly',
       :wildfly_group => 'wildfly'
-     }}
+     }}    
 
-    it { should contain_class('mymodule') }
+    it do
+      should contain_class('mymodule')
+    
+      should contain_package('httpd').with_ensure('present')
 
-    it { should contain_file('/opt/app/widlfly').with(
-          :ensure           => 'directory',
-          :owner            => 'widlfly',
-          :group            => 'wildfly',
-          :mode             => '0755'
-       )}
+      should contain_service('httpd').with(
+        :ensure => 'running',
+        :enable => 'true'
+      )
+      
+      should contain_file('/opt/app/wildfly').with(
+        :ensure  => 'directory'
+      )
+    end
   end
 
   context 'test some other event involving mymodule' do
@@ -27,12 +33,16 @@ describe 'mymodule' do
      }}
 
     it { should contain_class('mymodule') }
+    
+    it { should contain_package('httpd').with_ensure('present') }
 
-    it { should contain_file('/opt/widlfly').with(
-          :ensure           => 'directory',
-          :owner            => 'bob',
-          :group            => 'bob',
-          :mode             => '0755'
-       )}
+    it { should contain_service('httpd').with(
+      :ensure => 'running',
+      :enable => 'true'
+    ) }
+
+    it { should contain_file('/opt/wildfly').with(
+        :ensure  => 'directory'
+    )}
   end
 end
